a06692995658f483d079e356450bd12b816a90ba
Bruno Moleta <brunomoleta@pm.me>
2023-11-10 19:24:20 -0300

    feat/loginModal

---
+++ client/src/components/Login/Login.tsx
@@ -0,0 +1,40 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
+import { useUserContext } from "../../providers/UserContext";
+import { UserLogin } from "./UserLogin.tsx";
+import { UserSignUp } from "./UserSignUp.tsx";
+import { Cart } from "../../types/types";
+import {
+  DContent,
+  DOverLay,
+} from "../../styled-components/Modal.styles.tsx";
+import {X} from "react-feather";
+
+function Login() {
+  const { isLogOpen, setIsLogOpen, isSignUp } = useUserContext() as Cart;
+  return (
+    <Dialog.Root modal={true} open={isLogOpen} onOpenChange={setIsLogOpen}>
+      <Dialog.Portal>
+        <DOverLay />
+        <DContent>
+          {isSignUp ? UserLogin():UserSignUp()}
+            <Dialog.Close asChild>
+                <button
+                    style={{
+                        position: "absolute",
+                        top: "35px",
+                        right: "35px",
+                        backgroundColor: "inherit",
+                    }}
+                    aria-label="Close"
+                >
+                    <X size={20} />
+                </button>
+            </Dialog.Close>
+        </DContent>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
+
+export default Login;
