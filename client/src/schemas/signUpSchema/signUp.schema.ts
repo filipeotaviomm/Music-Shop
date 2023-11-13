4062c4ebf11bd41bc56b1b362e0d58f446821734
Bruno Moleta <brunomoleta@pm.me>
2023-11-13 20:08:05 -0300

    fix/signUpSchema

--- client/src/schemas/signUpSchema/signUp.schema.ts
+++ client/src/schemas/signUpSchema/signUp.schema.ts
@@ -2,6 +2,12 @@ import { z } from "zod";
 
 const signUpSchema = z
   .object({
+    firstName: z.string().refine((value) => value !== "", {
+      message: "Favor coloque seu o nome :)",
+    }),
+    lastName: z.string().refine((value) => value !== "", {
+      message: "Favor coloque seu sobrenome :)",
+    }),
     email: z
       .string()
 
