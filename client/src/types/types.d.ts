de3e8eabe90c172043f16921209fb0577d9df973
Bruno Moleta <brunomoleta@pm.me>
2023-11-13 12:02:06 -0300

    refactor/InputType

--- client/src/types/types.d.ts
+++ client/src/types/types.d.ts
@@ -1,30 +1,38 @@
 import React from "react";
+import { FieldError } from "react-hook-form";
 
 export interface ILogin {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
+
 export interface ISignUp extends ILogin {
-    confirmPassword: string;
+  confirmPassword: string;
+}
+
+export interface IInput extends React.InputHTMLAttributes<HTMLInputElement> {
+  error: FieldError | undefined;
+  label: string;
+  id: string;
 }
 
 export interface IContext {
-    cart: number;
-    setCart: React.Dispatch<React.SetStateAction<number>>
+  cart: number;
+  setCart: React.Dispatch<React.SetStateAction<number>>;
 
-    isLoggedIn: boolean;
-    setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  isLoggedIn: boolean;
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
 
-    isLoading: boolean;
-    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  isLoading: boolean;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
 
-    isLogOpen: boolean;
-    setIsLogOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isLogOpen: boolean;
+  setIsLogOpen: React.Dispatch<React.SetStateAction<boolean>>;
 
-    isSignUp: boolean;
-    setIsSignUp: React.Dispatch<React.SetStateAction<boolean>>;
+  isSignUp: boolean;
+  setIsSignUp: React.Dispatch<React.SetStateAction<boolean>>;
 
-    signUpRequest:(formData: ISignUp) => Promise<void>
-    loginRequest:(formData: ILogin) => Promise<void>
+  signUpRequest: (formData: ISignUp) => Promise<void>;
+  loginRequest: (formData: ILogin) => Promise<void>;
 }
 
