a32b39662a65d93385e9a16bf90c215cfbc70c34
Bruno Moleta <brunomoleta@pm.me>
2023-11-13 20:07:40 -0300

    feat/showHide password

--- client/src/components/Login/Forms/Input/Input.tsx
+++ client/src/components/Login/Forms/Input/Input.tsx
@@ -1,29 +1,65 @@
 import React, { ForwardedRef } from "react";
-import { IInput } from "../../../../types/types";
-import { Field } from "../../../../styled-components/Modal.styles.tsx";
+import { IContext, IInput } from "../../../../types/types";
+import {DefaultLabel, Field} from "../../../../styled-components/Modal.styles.tsx";
 import styled from "styled-components";
 import { colors } from "../../../../styled-components/root.ts";
+import { useUserContext } from "../../../../providers/UserContext";
+import { Eye, EyeOff } from "react-feather";
 
 const StyledInput = styled.input`
+  width: 100%;
   border-bottom: 2px solid ${colors.black};
+
+  &:focus {
+    border: none;
+    border-bottom: 2px solid ${colors.purple};
+    outline: 1px solid transparent;
+  }
+}
 `;
 const Span = styled.span`
   color: ${colors.red};
 `;
+const LabelWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Input = React.forwardRef(
   (
-    { error, id, label, ...delegated }: IInput,
+    { error, id, type = "text", label, ...delegated }: IInput,
     ref: ForwardedRef<HTMLInputElement>
   ) => {
+    const { changePasswordVisibility } = useUserContext() as IContext;
     return (
       <>
         <Field>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <label htmlFor={id}>{label}</label>
+          <LabelWrapper>
+            <DefaultLabel htmlFor={id}>
+              {label.charAt(0).toUpperCase() + label.slice(1)}
+            </DefaultLabel>
             <Span>{error && error.message}</Span>
+          </LabelWrapper>
+          <div style={{ position: "relative" }}>
+            <StyledInput type={type} id={id} ref={ref} {...delegated} />
+            {label.toLowerCase().includes("senha") && (
+              <button
+                style={{
+                  backgroundColor: "inherit",
+                  position: "absolute",
+                  top: "0",
+                  right: "0",
+                  zIndex: "1",
+                }}
+                onClick={(event) => {
+                  event.preventDefault();
+                  changePasswordVisibility();
+                }}
+              >
+                {type === "text" ? <Eye /> : <EyeOff />}
+              </button>
+            )}
           </div>
-          <StyledInput id={id} ref={ref} {...delegated} />
         </Field>
       </>
     );
--- client/src/providers/UserContext/UserProvider.tsx
+++ client/src/providers/UserContext/UserProvider.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {IContext, ILogin, ISignUp} from "../../types/types";
+import { IContext, ILogin, ISignUp } from "../../types/types";
 import { api } from "../../services/api.ts";
 
 const UserContext = React.createContext({});
@@ -16,21 +16,39 @@ function UserProvider(props: { children: React.ReactNode }) {
   const [isSignUp, setIsSignUp] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(false);
 
-  const signUpRequest = async (formData:ISignUp) => {
+  const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
+
+  const signUpRequest = async (formData: ISignUp) => {
+    const {
+      firstName: firstName,
+      lastName: lastName,
+      ...newFormData
+    } = formData;
+
+    const updatedFirstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+    const updatedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
+
+    // Concatenating the variables
+    const fullName = `${updatedFirstName} ${updatedLastName}`;
+    const updatedFormData = { ...newFormData, fullName };
     try {
-      await api.post("/users", formData);
+      await api.post("/users", updatedFormData);
     } catch (error) {
-      console.log("error")
-    }
-  };
-  const loginRequest = async (formData:ILogin) => {
-    try {
-      await api.post("/session", formData);
-    } catch (error) {
-      console.log("error")
+      console.log("error");
     }
   };
 
+  function changePasswordVisibility() {
+    setIsPasswordVisible(!isPasswordVisible);
+  }
+
+  const loginRequest = async (formData: ILogin) => {
+    try {
+      await api.post("/session", formData);
+    } catch (error) {
+      console.log("error");
+    }
+  };
 
   const values: IContext = {
     isSignUp,
@@ -50,6 +68,10 @@ function UserProvider(props: { children: React.ReactNode }) {
 
     isLoading,
     setIsLoading,
+
+    changePasswordVisibility,
+    isPasswordVisible,
+    setIsPasswordVisible,
   };
 
   return (
