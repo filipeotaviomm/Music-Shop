b0da4668dc79cb985a8355b9f58fca677ec8d1bd
Gustavo Lazarin <gustavolazarin03@gmail.com>
2023-11-14 17:28:49 -0300

    fix: signup errors solved

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,17 @@ export const app: Application = express();
 
 export const prisma = new PrismaClient();
 
+const cors = require("cors");
+
+const corsOptions = {
+    origin: 'http://localhost:5173', // Permite apenas solicitações deste domínio
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Permite incluir cookies na solicitação (se aplicável)
+    optionsSuccessStatus: 204, // Alguns navegadores (por exemplo, Chromium) podem enviar um status 204
+};
+
+app.use(cors(corsOptions));
+
 app.use(json());
 
 app.use("/", router);
