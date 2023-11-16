32a20fd52bdcac7e8bf730191c1a19206e3ce9ab
João Pedro <jp871157@gmail.com>
2023-11-16 15:13:58 -0300

    refactor: erased next from function parameters

--- server/src/controllers/payments.controller.ts
+++ server/src/controllers/payments.controller.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { createUserCardService } from "../services/payments.service";
 
-export const createUserCardController = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+export const createUserCardController = async (req: Request, res: Response): Promise<Response> => {
   const newUserCard = await createUserCardService(req.body, Number(req.params.userId));
 
   return res.status(201).json(newUserCard);
