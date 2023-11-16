c95ef5af5088b4306929a4219b9d60c93a013c59
Gustavo Lazarin <gustavolazarin03@gmail.com>
2023-11-16 18:36:37 -0300

    feat: finish address CRUD  and schemas (2/2)

--- server/src/routers/index.router.ts
+++ server/src/routers/index.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRouter } from "./users.router";
 import { sessionRouter } from "./session.router";
+import { addressRouter } from "./addresses.router";
 
 export const router: Router = Router();
 
 router.use("/users", userRouter);
 router.use("/login", sessionRouter);
+router.use("/addresses", addressRouter);
