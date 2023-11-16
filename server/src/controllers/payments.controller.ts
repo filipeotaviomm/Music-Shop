df524f0529f552a31f4c06cb0b8953e5c9ea6ff8
João Pedro <jp871157@gmail.com>
2023-11-16 15:30:09 -0300

    feat: created update cards functions

--- server/src/controllers/payments.controller.ts
+++ server/src/controllers/payments.controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
-import { createUserCardService } from "../services/payments.service";
+import { createUserCardService, updateUserCardService } from "../services/payments.service";
 
 export const createUserCardController = async (req: Request, res: Response): Promise<Response> => {
   const newUserCard = await createUserCardService(req.body, Number(req.params.userId));
 
   return res.status(201).json(newUserCard);
+}
+
+export const updateUserCardController = async (req: Request, res: Response): Promise<Response> => {
+  const updatedCard = await updateUserCardService(req.body, Number(req.params.cardId));
+
+  return res.status(200).json(updatedCard);
 }
--- server/src/services/payments.service.ts
+++ server/src/services/payments.service.ts
@@ -1,8 +1,17 @@
 import { prisma } from "../app"
-import { Card, CardCreate } from "../interfaces/cards.interface"
+import { Card, CardCreate, UpdateCard } from "../interfaces/cards.interface"
 
 export const createUserCardService = async (cardInfo: CardCreate, userId: number ): Promise<Card> => {
   const newCard: Card = await prisma.card.create({ data: {...cardInfo, userId } });
 
   return newCard;
+}
+
+export const updateUserCardService = async (cardInfo: UpdateCard, cardId: number): Promise<Card> => {
+  const updatedCard: Card = await prisma.card.update({
+    where: { id: cardId },
+    data: { ...cardInfo },
+  });
+
+  return updatedCard;
 }
