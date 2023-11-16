d5ab30896d19412de18e1c00d920d066fabdecc8
Bruno Moleta <brunomoleta@pm.me>
2023-11-16 20:39:14 -0300

    styles/modalStyles title and subtitle

--- client/src/components/LoginOrSignUp/LoginOrSignUp.tsx
+++ client/src/components/LoginOrSignUp/LoginOrSignUp.tsx
@@ -1,33 +1,42 @@
-import * as Dialog from "@radix-ui/react-dialog";
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
-
-import {DClose, DTitle} from "../../styled-components/Modal.styles.tsx";
+import {
+  DClose,
+  DDescription,
+  DTitle,
+} from "../../styled-components/Modal.styles.tsx";
 import FormSignUp from "../Login/Forms/FormSignUp";
 import AlternateForm from "../Login/AlternateForm";
 import FormLogin from "../Login/Forms/FormLogin";
 
-function LoginOrSignUp(isSignUp:boolean) {
+function LoginOrSignUp(isSignUp: boolean) {
   const LogInInfo = {
-    title: "LOGIN",
-    description: "Faça login na sua conta",
+    title: "Bem-vinda(o) de volta",
+    description: "Por favor coloque seus dados para comprar o melhor equipamento por um preço justo",
     question: "Não tens conta com a gente?",
     buttonTitle: "CADASTRAR",
   };
   const SignUpInfo = {
-    title: "CADASTRO",
-    description: "Faça seu cadastro",
+    title: "Seja bem-vinda(o)",
+    description: "Cadastre-se conosco e encontre seu equipamento ",
     question: "Tens conta com a gente?",
     buttonTitle: "LOGIN",
   };
 
   return (
     <>
-      <DTitle>{isSignUp ? SignUpInfo.title : LogInInfo.title}</DTitle>
-      <VisuallyHidden asChild>
-        <Dialog.Description>
+      <div
+        style={{
+          display: "flex",
+          flexFlow: "column",
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          gap: "24px",
+        }}
+      >
+        <DTitle>{isSignUp ? SignUpInfo.title : LogInInfo.title}</DTitle>
+        <DDescription>
           {isSignUp ? SignUpInfo.description : LogInInfo.description}
-        </Dialog.Description>
-      </VisuallyHidden>
+        </DDescription>
+      </div>
 
       {isSignUp ? <FormSignUp /> : <FormLogin />}
       {isSignUp
