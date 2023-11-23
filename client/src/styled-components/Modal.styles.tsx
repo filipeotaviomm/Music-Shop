f9109a08c638cb795921d0067a153405d20532ab
Bruno Moleta <brunomoleta@pm.me>
2023-11-23 18:34:41 -0300

    refactor(Modal)/simplify Modal subtitle

--- client/src/styled-components/Modal.styles.tsx
+++ client/src/styled-components/Modal.styles.tsx
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 import * as Dialog from "@radix-ui/react-dialog";
-import {colors, fontSize} from "./root.ts";
-import {StyledButton} from "./Button.styles.ts";
-
+import { colors, fontSize } from "./root.ts";
 
 export const Field = styled.fieldset`
   display: flex;
@@ -11,22 +9,20 @@ export const Field = styled.fieldset`
   border-radius: 8px;
   border: 2px solid ${colors.offWhite};
   outline: 1px solid ${colors.offWhite}
-  padding-block: 16px;
-  display: flex;
   flex-flow: column;
   gap: 8px;
   padding-inline: 20px;
   padding-block: 16px;
 
-  &:focus-within{
+  &:focus-within {
     border: 2px solid ${colors.purple};
   }
 `;
 
 export const DefaultLabel = styled.label`
-  width: 100%;
+  width: fit-content;
   font-weight: 500;
-`
+`;
 
 export const DOverLay = styled(Dialog.Overlay)`
   background-color: rgba(0 0 0 / 0.5);
@@ -41,34 +37,33 @@ export const DOverLay = styled(Dialog.Overlay)`
 `;
 
 export const DDescription = styled(Dialog.Description)`
-  color: ${colors.grey70}
-`
+  color: ${colors.grey70};
+`;
 
 export const DContent = styled(Dialog.Content)`
   display: grid;
   place-items: center;
-  gap: 32px;
-  @media (max-width: 768px) {
-    gap: 20px
-  }
+  gap: clamp(1svh, 2svh, 32px);
+
 
   background-color: white;
   border-radius: 8px;
   box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px hsl(206 22% 7% / 20%) 0 10px
     20px -15px;
-  
+
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  
+
   width: 90vw;
   max-width: 600px;
   height: auto;
+  max-height: 95dvh;
 
-  padding-block: 64px;
-  padding-inline: 40px;
-  
+  padding-block: clamp(2%, 5%, 64px);
+  padding-inline: clamp(2%, 5%, 40px);
+
   & > * {
     width: 100%;
   }
@@ -88,10 +83,5 @@ export const DClose = styled(Dialog.Close)`
 
 export const Form = styled.form``;
 
-export const SendBtn = styled(StyledButton)`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+
 
