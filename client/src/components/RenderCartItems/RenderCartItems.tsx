244508fc7af92c9d02493f6ce2059281906daf85
Bruno Moleta <brunomoleta@pm.me>
2023-11-23 13:39:13 -0300

    feat/CartModal buttons

--- client/src/components/RenderCartItems/RenderCartItems.tsx
+++ client/src/components/RenderCartItems/RenderCartItems.tsx
@@ -1,19 +1,62 @@
 import CartItem from "../CartItem";
-import { useCartContext } from "../../providers/UserContext";
+import { useCartContext, useUserContext } from "../../providers/UserContext";
 import { ICartContext } from "../../types/cart";
 import { nanoid } from "nanoid";
-import { InlineButton } from "../../styled-components/Button.styles.ts";
+import {
+  ModalBottonButton,
+  SendBtn,
+  WarningInlineButton,
+} from "../../styled-components/Button.styles.ts";
 import styled from "styled-components";
 import { H3 } from "../../styled-components/Typography.styles.ts";
-import {fontSize} from "../../styled-components/root.ts";
+import { colors, fontSize } from "../../styled-components/root.ts";
+import { IUserContext } from "../../types/user";
+import { useNavigate } from "react-router-dom";
 
 const CartOl = styled.ol`
   margin-block: 16px;
+  max-height: 550px;
+  overflow-y: auto;
+  padding-inline-end: 8px;
+  @media (min-width: 550px) {
+    padding-inline-end: unset;
+  }
+`;
+
+const Buttons = styled.div`
+  display: grid;
+  gap: 24px;
+  @media (min-width: 550px) {
+    display: flex;
+  }
+`;
+const FinalPrice = styled(H3)`
+  font-size: ${fontSize.link};
+  font-weight: 600;
+`;
+const Wrapper = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-rows: auto 50px 120px;
+  height: 100%;
+  @media (min-width: 550px) {
+    grid-template-rows: auto 50px 65px;
+  }
 `;
 
 function RenderCartItems() {
-  const { cart, cleanCart } = useCartContext() as ICartContext;
+  const { cart, cleanCart, setIsCartModalOpen, isCartModalOpen } =
+    useCartContext() as ICartContext;
+  const { setIsLogOpen, isLogOpen } = useUserContext() as IUserContext;
 
+  function LogToBuy() {
+    setIsCartModalOpen(!isCartModalOpen);
+    setIsLogOpen(!isLogOpen);
+  }
+
+  const { token } = useUserContext() as IUserContext;
+
+  const navigate = useNavigate();
   const subTotal: number = cart
     ? cart.reduce((total, item) => {
         const price = item.product.price;
@@ -27,12 +70,9 @@ function RenderCartItems() {
     minimumFractionDigits: 2,
   }).format(subTotal);
 
-  const FinalPrice = styled(H3)`
-    font-size: ${fontSize.link};
-    font-weight: 600;
-  `
   return (
-    <>
+    <Wrapper>
+      <WarningInlineButton onClick={cleanCart}>limpar</WarningInlineButton>
       {/*<ol style={{overflow: "auto"}}>*/}
       <CartOl>
         {cart &&
@@ -44,12 +84,28 @@ function RenderCartItems() {
             />
           ))}
       </CartOl>
-      <div style={{ display: "flex", justifyContent:"space-between" }}>
-        <InlineButton onClick={cleanCart}>limpar</InlineButton>
-
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <H3 style={{ fontSize: fontSize.link }}>Subtotal:</H3>
         <FinalPrice>{finalPrice}</FinalPrice>
       </div>
-    </>
+      <Buttons>
+        <ModalBottonButton
+          onClick={() => setIsCartModalOpen(!isCartModalOpen)}
+          color={colors.purple}
+          id={colors.offWhite}
+        >
+          Continuar comprando
+        </ModalBottonButton>
+        <SendBtn
+          style={{ marginBlock: "0" }}
+          onClick={() => {
+            token ? navigate("/makeorder") : LogToBuy();
+          }}
+        >
+          Finalizar compra
+        </SendBtn>
+      </Buttons>
+    </Wrapper>
   );
 }
 
