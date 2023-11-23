111eee6b161eadb3225bef8b874ac8877ffed482
Bruno Moleta <brunomoleta@pm.me>
2023-11-23 18:35:56 -0300

    styles/modal CartItems

--- client/src/components/RenderCartItems/RenderCartItems.tsx
+++ client/src/components/RenderCartItems/RenderCartItems.tsx
@@ -1,19 +1,72 @@
 import CartItem from "../CartItem";
-import { useCartContext } from "../../providers/UserContext";
+import { useCartContext, useUserContext } from "../../providers/UserContext";
 import { ICartContext } from "../../types/cart";
 import { nanoid } from "nanoid";
-import { InlineButton } from "../../styled-components/Button.styles.ts";
+import {
+  ModalBottonButton,
+  SendBtn,
+} from "../../styled-components/Button.styles.ts";
 import styled from "styled-components";
 import { H3 } from "../../styled-components/Typography.styles.ts";
-import {fontSize} from "../../styled-components/root.ts";
+import { colors, fontSize } from "../../styled-components/root.ts";
+import { IUserContext } from "../../types/user";
+import { useNavigate } from "react-router-dom";
 
 const CartOl = styled.ol`
   margin-block: 16px;
+  overflow-y: auto;
+  padding-inline-end: clamp(8px, 5%, 32px);
+  height: 100%;
+  margin-block-start: 40px;
+  @media (min-width: 550px) {
+    max-height: 450px;
+
+    margin-block: 0;
+  }
 `;
 
-function RenderCartItems() {
-  const { cart, cleanCart } = useCartContext() as ICartContext;
+const Buttons = styled.div`
+  display: grid;
+  gap: 24px;
+  @media (min-width: 550px) {
+    display: flex;
+  }
+`;
+const FinalPrice = styled(H3)`
+  font-size: ${fontSize.link};
+  font-weight: 600;
+`;
+const Wrapper = styled.div`
+  display: grid;
+  gap: clamp(1svh, 3svh, 16px);
+  grid-template-rows: auto auto;
+  height: 85svh;
+  @media (min-width: 550px) {
+    height: 100%;
 
+  }
+`;
+
+const BottonInfo = styled.div`
+  margin-block-start: 32px;
+  display: flex;
+  flex-flow: column;
+  gap: 16px;
+`
+
+function RenderCartItems() {
+  const { cart, setIsCartModalOpen, isCartModalOpen } =
+    useCartContext() as ICartContext;
+  const { setIsLogOpen, isLogOpen } = useUserContext() as IUserContext;
+
+  function LogToBuy() {
+    setIsCartModalOpen(!isCartModalOpen);
+    setIsLogOpen(!isLogOpen);
+  }
+
+  const { token } = useUserContext() as IUserContext;
+
+  const navigate = useNavigate();
   const subTotal: number = cart
     ? cart.reduce((total, item) => {
         const price = item.product.price;
@@ -27,13 +80,8 @@ function RenderCartItems() {
     minimumFractionDigits: 2,
   }).format(subTotal);
 
-  const FinalPrice = styled(H3)`
-    font-size: ${fontSize.link};
-    font-weight: 600;
-  `
   return (
-    <>
-      {/*<ol style={{overflow: "auto"}}>*/}
+    <Wrapper>
       <CartOl>
         {cart &&
           cart.map((item) => (
@@ -44,12 +92,35 @@ function RenderCartItems() {
             />
           ))}
       </CartOl>
-      <div style={{ display: "flex", justifyContent:"space-between" }}>
-        <InlineButton onClick={cleanCart}>limpar</InlineButton>
-
-        <FinalPrice>{finalPrice}</FinalPrice>
-      </div>
-    </>
+      <BottonInfo >
+        <div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <H3 style={{ fontSize: fontSize.link }}>Subtotal:</H3>
+            <FinalPrice>{finalPrice}</FinalPrice>
+          </div>
+          <p style={{ color: colors.grey70, fontSize: fontSize.smallLink }}>
+            O frete é adicionado a seguir :)
+          </p>
+        </div >
+        <Buttons>
+          <ModalBottonButton
+            onClick={() => setIsCartModalOpen(!isCartModalOpen)}
+            color={colors.purple}
+            id={colors.offWhite}
+          >
+            Continuar comprando
+          </ModalBottonButton>
+          <SendBtn
+            style={{ marginBlock: "0" }}
+            onClick={() => {
+              token ? navigate("/makeorder") : LogToBuy();
+            }}
+          >
+            Finalizar compra
+          </SendBtn>
+        </Buttons>
+      </BottonInfo>
+    </Wrapper>
   );
 }
 
