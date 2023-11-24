1ad2431b8d551a859e051d917907ddff8ae50093
Gustavo Lazarin <gustavolazarin03@gmail.com>
2023-11-23 21:26:31 -0300

    feat: update Address provider

--- client/src/providers/UserContext/AddressProvider.tsx
+++ client/src/providers/UserContext/AddressProvider.tsx
@@ -1,6 +1,7 @@
-import React, {createContext, ReactNode, useEffect, useState} from "react";
-import {toast} from "react-toastify";
-import { AddressValues, IAddressContext } from "../../types/address";
+import React, {createContext, ReactNode, useState} from "react";
+import { IAddressContext, IAddress, IAddressForm,  } from "../../types/address";
+import { api } from "../../services/api";
+import { toast } from "react-toastify";
 
 export const AddressContext = createContext({});
 
@@ -8,12 +9,49 @@ const useAddressContext = () => React.useContext(AddressContext);
 
 const AddressProvider = (props: { children: ReactNode }) => {
     
-    const [ addresses, setAddresses ] = useState<IAddressContext[]>([]);
+    const [ addresses, setAddresses ] = useState<IAddress[]>([]);
+
+    const [ isCreateAddressModalOpen, setIsCreateAddressModalOpen ] = useState<boolean>(false);
     
-    const values: AddressValues = {
-        addresses
+    async function createAddressRequest(formData: IAddressForm) {
+        const token = JSON.parse(localStorage.getItem("@TOKEN")!);
+
+        try {
+            const response = await api.post("/addresses", formData, {
+                headers: {Authorization: `Bearer ${token}`}
+            });
+            toast.success(`Endereço "${formData.name}" cadastrado com sucesso!`)
+
+            getAllAddresses();
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsCreateAddressModalOpen(false);
+        }
     };
     
+    async function getAllAddresses() {
+        const token = JSON.parse(localStorage.getItem("@TOKEN")!);
+        try {
+            const {data} = await api.get("/addresses", {
+                headers: {Authorization: `Bearer ${token}`}
+            });
+            setAddresses(data);
+        } catch (error) {
+            
+        }
+
+    
+    };
+
+    const values: IAddressContext = {
+        addresses,
+        isCreateAddressModalOpen,
+        setIsCreateAddressModalOpen,
+        createAddressRequest,
+        getAllAddresses
+    };
+
     return (
         <AddressContext.Provider value={values}>{props.children}</AddressContext.Provider>
         );
--- client/src/types/address.d.ts
+++ client/src/types/address.d.ts
@@ -1,4 +1,4 @@
-export interface IAddressContext {
+export interface IAddress {
     id: number,
     name: string,
     street: string,
@@ -10,7 +10,28 @@ export interface IAddressContext {
     complement: string,
     userId: number
   }
+  
+  export interface IAddressForm {
+    "name": string,
+    "city": string,
+    "street": string,
+    "number": number,
+    "neihborhood": string,
+    "complement": string,
+    "zip": string,
+    "state": string
+  }
+  
+  export interface IAddressContext {
+    addresses: IAddress[]
 
-  export interface AddressValues {
-    addresses: IAddressContext[]
+    isCreateAddressModalOpen: boolean
+    setIsCreateAddressModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+
+    createAddressRequest: (formData: IAddressForm) => Promise<void>
+    getAllAddresses: () => Promise<void>
+  }
+
+  export interface IAddressCard {
+    address: IAddress
   }
