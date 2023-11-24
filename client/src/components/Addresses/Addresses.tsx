c13a126c2bc6ddbe0f92c5580fd6d163787a8f3e
Gustavo Lazarin <gustavolazarin03@gmail.com>
2023-11-23 21:29:24 -0300

    feat: add Address imports and returns

--- client/src/components/Addresses/Addresses.tsx
+++ client/src/components/Addresses/Addresses.tsx
@@ -1,41 +1,81 @@
 import styled from "styled-components";
 import { H1, H2 } from "../../styled-components/Typography.styles";
 import { useAddressContext } from "../../providers/UserContext/AddressProvider";
-import { AddressValues } from "../../types/address";
-import NotFound from "../../assets/No-Order.svg"
-import Button from "../Button";
+import { IAddressContext } from "../../types/address";
+import NotFound from "../../assets/No-Order.svg";
+import { MdOutlineAddCircleOutline } from "react-icons/md"
+import CreateAddressForm from "./Form/CreateAddressForm";
+import { useEffect } from "react";
+import { AddressCard } from "./AddressCard/AddressCard";
 
-const AddressesContainer = styled.div`
-  width: 495px;
-  padding: 20px;
+const AddressContent = styled.div`
+  width: 100%;
+  max-width: 40rem;
+  margin-top: 20px;
+
   display: flex;
   align-items: center;
-  justify-content: center;
+  flex-direction: column;
   gap: 10px;
-  background-color: #8080803b;
-  width: fit-content;
+  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
 `;
 
+const AddressHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const AddAddressBtn = styled.button`
+padding: 16px;
+border-radius: 20px;
+transition: .05s;
+  
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+
+  &:hover{
+    outline: 2px solid hsla(242, 62%, 56%, 1);
+    background-color: #fff;
+  }
+`;
 
 function Addresses() {
 
-  const { addresses } = useAddressContext() as AddressValues;
+    const { addresses, isCreateAddressModalOpen, setIsCreateAddressModalOpen, getAllAddresses } = useAddressContext() as IAddressContext;
+
+    useEffect(() => {
+      getAllAddresses();
+    }, []);
+
 
   return (
     <>
-      <div>
+      <AddressHeader>
         <H1>ENDEREÇOS</H1>
-      </div>
+        <AddAddressBtn onClick={() => setIsCreateAddressModalOpen(!isCreateAddressModalOpen)}>
+          <MdOutlineAddCircleOutline size="18" />
+          Endereço
+          </AddAddressBtn>
+      </AddressHeader>
 
-      <AddressesContainer>
-      {addresses.length > 0 ? <div>TEMOS ENDEREÇOS!</div> : (
-        <>
-          <img src={NotFound} style={{width: 'fit-content'}}/>
-          <H2>Nada por aqui!</H2>
-        </>
-      )}
+      <AddressContent>
 
-      </AddressesContainer>
+        {addresses.length > 0 ? (
+          addresses.map((address) => (
+            <AddressCard key={address.id} address={address}/>
+            ))
+            ) : (
+              <>
+            <img src={NotFound} style={{width: 'fit-content'}}/>
+            <H2>Nada por aqui!</H2>
+          </>
+        )}
+        
+        {isCreateAddressModalOpen ? <CreateAddressForm/> : null}
+
+      </AddressContent>
     </>
   )
 }
