4b4bad98614ceb1c460a709b30453b1226cbc40e
Bruno Moleta <brunomoleta@pm.me>
2023-11-27 23:42:23 -0300

    deploy/starts with...

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,20 +1,21 @@
 import "express-async-errors";
+
 import express, { Application, json } from "express";
 import { PrismaClient } from "@prisma/client";
 import { handleErrors } from "./middlewares/handleErrors.middleware";
 import { router } from "./routers/index.router";
 
+const cors = require("cors");
+
 export const app: Application = express();
 
 export const prisma = new PrismaClient();
 
-const cors = require("cors");
-
 const corsOptions = {
-    origin: ['https://durvalmusicshop-63y16yloq-brunomoleta.vercel.app'],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    credentials: true,
-    optionsSuccessStatus: 204
+  origin: origin.startsWith("https://durvalmusicshop"),
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  credentials: true,
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
